c6adfb1b84d7db71cd74a6b3953c464690b938f5
abitamilan75-lang <abitamilan75@gmail.com>
2026-02-12 10:40:10 +0530

    Add files via upload

---
+++ check which is grerater than print.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+   int a,b,c;
+   printf("enter a character : ");
+   scanf("%d%d%d",&a,&b,&c);
+    
+     
+   if (a>b) 
+   {
+      printf(" %d  is greater than") ;
+   }
+    else if (b>c) {
+     printf(" %d  is greater than");
+    }
+    else (c>a);
+    {
+        printf("%d  is greater than");
+    }
+       
+   
+
+    return 0;
+}
