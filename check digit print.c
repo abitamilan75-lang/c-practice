2aebf1509cd42a67342c844a37c4786c14d05b44
abitamilan75-lang <abitamilan75@gmail.com>
2026-02-12 13:50:16 +0530

    Add files via upload

---
+++ check digit print.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c",&a);
+    if(a>=48&&a<=57){
+    printf("digit");
+    }
+    else {
+        printf("not digital");
+    }
+    
+    
+
+    return 0;
+}
