87494c15a58fe4daa8638c2997323c5a19f1b90c
abitamilan75-lang <abitamilan75@gmail.com>
2026-02-14 10:20:25 +0530

    Add files via upload

---
+++ factorial calculator print.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main() {
+    int i,n,fact=1;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for(i=n;i>1;i--)  {      
+        fact= fact*i;
+        
+    }
+    printf("%d ",fact);
+    
+
+    return 0;
+
+}
